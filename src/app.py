d9f1de8f27f2f123c5b0cc4afb47b693066480fa
RonZatkovsky <167799853+RonZatkovsky@users.noreply.github.com>
2024-10-16 19:53:03 +0000

    unsaved changes

--- src/app.py
+++ src/app.py
@@ -38,12 +38,8 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+    user=User.query.get(1)
+    return jsonify(user.serialize()),200
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -70,32 +66,76 @@ def get_favs():
     all_favs=list(map(lambda x: x.serialize(),favs))
     return jsonify(all_favs),200
 
-@app.route('/favorite/planet/',methods=['POST'])
+@app.route('/favorite',methods=['POST'])
 def add_fav():
-    new_fav=Favorites(
-        name=request.json["name"],
-        category=request.json["category"],
-        user_id=request.json["user_id"],
-        planets_id=request.json["planets_id"],
-    )
+    if request.json['category']=='planets':
+        new_entity=Planets(
+            name= request.json["name"],
+            climate= request.json["climate"],
+            gravity= request.json["gravity"],
+            population= request.json["population"],
+            terrain= request.json["terrain"],
+        )
+        db.session.add(new_entity)
+        db.session.commit()
+        planets_info=Planets.query.filter_by(name=request.json["name"]).first()
+        new_fav=Favorites(
+            name=request.json["name"],
+            category=request.json["category"],
+            user_id=request.json["user_id"],
+            planets_id=planets_info.id,
+        )
+    elif request.json['category']=='people':
+        new_entity=People(
+            name=request.json["name"],
+            gender=request.json["gender"],
+            birth_year=request.json["birth_year"],
+            height=request.json["height"],
+            mass=request.json["mass"],
+        )
+        db.session.add(new_entity)
+        db.session.commit()
+        people_info=People.query.filter_by(name=request.json["name"]).first()
+        new_fav=Favorites(
+            name=request.json["name"],
+            category=request.json["category"],
+            user_id=request.json["user_id"],
+            people_id=people_info.id,
+        )
+    # elif request.json['category']=='vehicles':
+    #     new_entity=Vehicles(
+            
+    #     )
+    #     new_fav=Favorites(
+    #         name=request.json["name"],
+    #         category=request.json["category"],
+    #         user_id=request.json["user_id"],
+    #         vehicles_id=request.json["entity_id"],
+    #     )
     db.session.add(new_fav)
     db.session.commit()
     favs=Favorites.query.all()
     return jsonify(list(map(lambda x: x.serialize(),favs)))
 
-@app.route('/users/planets',methods=["POST"])
-def add_planet():
-    new_planet=Planets(
-            name= request.json['name'],
-            climate= request.json['climate'],
-            gravity= request.json['gravity'],
-            population= request.json['population'],
-            terrain= request.json['terrain']
-    )
-    db.session.add(new_planet)
+@app.route('/favorite/<string:category>/<int:entity_id>/<int:user_id>',methods=["DELETE"])
+def del_fav(category,entity_id,user_id):
+    if(category=='planets'):
+        record=Favorites.query.filter_by(category=category,planets_id=entity_id,user_id=user_id).first()
+        entity=Planets.query.filter_by(id=entity_id).first()
+        
+    elif(category=='people'):
+        record=Favorites.query.filter_by(category=category,people_id=entity_id,user_id=user_id).first()
+        entity=People.query.filter_by(id=entity_id).first()
+    db.session.delete(record)
+    db.session.delete(entity)
     db.session.commit()
-    planets=Planets.query.all()
-    return jsonify(list(map(lambda x: x.serialize(),planets)))
+    return "Deleted "+entity.name
+
+@app.route('/planets',methods=["GET"])
+def get_planets():
+    query=Planets.query.all()
+    planets=list(map(lambda x: x.serialize(),query))
+    return jsonify(planets)
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
